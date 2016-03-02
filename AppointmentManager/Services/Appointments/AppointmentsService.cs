708ab1bc56fb5483c49525ff0e414d9cab26ea47
devcatch <bonde.art@gmail.com>
2016-03-02 18:05:05 +0100

    Added AppointmentsService

--- AppointmentManager/Services/Appointments/AppointmentsService.cs
+++ AppointmentManager/Services/Appointments/AppointmentsService.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using AppointmentManager.Services.Rest;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Threading;
 
-namespace AppointmentManager
+[assembly: Xamarin.Forms.Dependency (typeof(AppointmentManager.Services.Appointments.AppointmentsService))]
+
+namespace AppointmentManager.Services.Appointments
 {
-	public class AppointmentsService
+	public class AppointmentsService : IAppointmentsService
 	{
-		public AppointmentsService ()
+		readonly IRestService _restService = DependencyService.Get<IRestService> ();
+
+		#region IAppointmentsService implementation
+
+		public Task<List<Appointment>> GetAppointments (string userId)
 		{
+			return _restService.GetAsync<List<Appointment>> (
+				$"http://service.appointments.com/users/{userId}/appointments",
+				CancellationToken.None
+			);
 		}
+
+		#endregion
+		
 	}
 }
 
--- AppointmentManager/Services/Appointments/IAppointmentsService.cs
+++ AppointmentManager/Services/Appointments/IAppointmentsService.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace AppointmentManager
+namespace AppointmentManager.Services.Appointments
 {
 	public interface IAppointmentsService
 	{
+		Task<List<Appointment>> GetAppointments(string userId);
 	}
 }
 
