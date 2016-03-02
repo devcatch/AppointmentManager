c3d690c6758323bc9aa673727d8658b1157be1d8
devcatch <bonde.art@gmail.com>
2016-03-02 13:43:27 +0100

    Added AppointmentsPage with binding

---
+++ AppointmentManager/ViewModels/AppointmentsPageViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+
+namespace AppointmentManager
+{
+	public class AppointmentsPageViewModel : BaseViewModel
+	{ 
+		public AppointmentsPageViewModel ()
+		{
+		}
+
+		List<Appointment> _appointments;
+		public List<Appointment> Appointments {
+			get { return _appointments; }
+			set { SetProperty (ref _appointments, value); }
+		}
+
+		string _labelText;
+		public string LabelText {
+			get { return _labelText; }
+			set { SetProperty (ref _labelText, value); }
+		}
+
+		Command _reloadCommand;
+
+		public Command ReloadCommand {
+			get {
+				return _reloadCommand ??
+					(_reloadCommand = new Command (async () => {
+						Inactive = false;
+						await DoReload ();
+						Inactive = true;
+					}, () => Inactive));
+			}
+		}
+
+		System.Random random = new System.Random();
+
+		public async Task DoReload ()
+		{
+			LabelText = "dasdadasdasADSdadsadsadas DSas as ";
+			System.Diagnostics.Debug.WriteLine("UPDATE");
+
+			Appointments = new List<Appointment> ()
+			{
+				new Appointment(){
+					Id = $"{random.Next()}",
+					DoctorId = "2"
+				},
+
+				new Appointment(){
+					Id = $"{random.Next()}",
+					DoctorId = "3"
+				},
+
+				new Appointment(){
+					Id = $"{random.Next()}",
+					DoctorId = "5"
+				},
+
+				new Appointment(){
+					Id = $"{random.Next()}",
+					DoctorId = "7"
+				},
+			};
+		}
+	}
+}
+
---
+++ AppointmentManager/ViewModels/BaseViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AppointmentManager
+{
+	public class BaseViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected bool SetProperty<T> (ref T storage, T value, [CallerMemberName] String propertyName = null)
+		{
+			if (object.Equals (storage, value))
+				return false;
+
+			storage = value;
+			OnPropertyChanged (propertyName);
+			return true;
+		}
+
+		protected virtual void OnPropertyChanged ([CallerMemberName] string propertyName = null)
+		{
+			var handler = PropertyChanged;
+			if (handler != null)
+				handler (this, new PropertyChangedEventArgs (propertyName));
+		}
+
+		bool _inactive = true;
+
+		public bool Inactive { 
+			get { 
+				return _inactive;
+			} 
+			set {
+				if (_inactive != value) {
+					_inactive = value;
+					ChangeCanExecute ();
+					OnPropertyChanged ();
+				}
+			}
+		}
+
+		public virtual void ChangeCanExecute ()
+		{
+
+		}
+	}
+}
+
