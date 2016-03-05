fd4cb767c808282a2a7351230572273416613189
devcatch <bonde.art@gmail.com>
2016-03-05 18:40:17 +0100

    Added test for push from ApointmentsViewModel

--- AppointmentManagerTest/ViewModels/AppointmentsPageViewModelTests.cs
+++ AppointmentManagerTest/ViewModels/AppointmentsPageViewModelTests.cs
@@ -3,6 +3,7 @@ using Moq;
 using AppointmentManager;
 using AppointmentManager.Services.Appointments;
 using System.Collections.Generic;
+using Xamarin.Forms;
 
 namespace AppointmentManagerTestsWithMock
 {
@@ -10,6 +11,7 @@ namespace AppointmentManagerTestsWithMock
 	public class AppointmentsPageViewModelTests
 	{
 		Mock<IAppointmentsService> _appointmentsServiceMock;
+		Mock<INavigation> _navigationMock;
 
 		[SetUp]
 		public void SetUp()
@@ -38,14 +40,29 @@ namespace AppointmentManagerTestsWithMock
 
 			_appointmentsServiceMock.Setup (m => m.GetAppointments (It.IsAny<string> ())).ReturnsAsync (list);
 			var vm = new AppointmentsPageViewModel (_appointmentsServiceMock.Object, null);
-			await vm.DoReload ();
 
 			// Act
+			await vm.DoReload ();
+
+			// Assert
 			_appointmentsServiceMock.Verify(moq => moq.GetAppointments(It.IsAny<string>()),
 				Times.Once);
 
-			// Assert
 			Assert.IsTrue (vm.Appointments == list);
+		}
+
+		[Test]
+		public void ViewModelPushPageAfteSelectItem ()
+		{
+			// Arrange
+			_navigationMock.Setup(moq => moq.PushAsync(It.IsAny<Page>()));
+			var vm = new AppointmentsPageViewModel (null, _navigationMock.Object);
+
+			// Act
+			vm.ItemSelectedCommand.Execute(It.IsAny<Appointment>());
+
+			// Assert
+			_navigationMock.Verify(moq => moq.PushAsync(It.IsAny<Page>()), Times.Once);
 		}
 	}
 }
