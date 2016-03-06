0e525a140d1b32b943f3a4dc18b997aa1c17e60f
devcatch <bonde.art@gmail.com>
2016-03-06 17:57:48 +0100

    Added map

--- AppointmentManager/Constants.cs
+++ AppointmentManager/Constants.cs
@@ -6,7 +6,7 @@
 		public const string UserId = "3191692";
 		public static bool IsStubData = true;
 		public const string Appointments = "[{ \"Id\":\"1233\", \"time\": 1456909260, \"DoctorId\":\"9751551\", \"Notes\":\"Do some thing\", \"DoctorType\":\"Ear doctor\" },{ \"Id\":\"3219\", \"time\": 1456909260, \"DoctorId\":\"9751551\", \"Notes\":\"Ear doctor\", \"DoctorType\":\"Ear doctor\" },{ \"Id\":\"387961\", \"time\": 1456909260, \"DoctorId\":\"9751551\", \"Notes\":\"Ear doctor\", \"DoctorType\":\"Ear doctor\" },{ \"Id\":\"4698615\", \"time\": 1456909260, \"DoctorId\":\"8977977\", \"Notes\":\"Audiologist\", \"DoctorType\":\"Audiologist\" },{ \"Id\":\"0085715\", \"time\": 1456909260, \"DoctorId\":\"8977977\", \"Notes\":\"Audiologist\", \"DoctorType\":\"Audiologist\" },{ \"Id\":\"75251\", \"time\": 1456909260, \"DoctorId\":\"8977977\", \"Notes\":\"Audiologist\", \"DoctorType\":\"Audiologist\" }]";
-		public const string DoctorInfo = "{ \"Id\": \"2331111\", \"Name\": \"Dr. Paul De Lacost\", \"OpeningHours\": [ \"Mo 9.00 - 18.00\", \"Mo 9.00 - 18.00\", \"Mo 9.00 - 18.00\", \"Mo 9.00 - 18.00\", \"Mo 9.00 - 18.00\" ], \"Street\": \"Some street\", \"PostalCode\": 1204, \"City\": \"Berlin\", \"Longitude\": 10.1, \"Latitude\": 15.1\n}";
+		public const string DoctorInfo = "{ \"Id\": \"2331111\", \"Name\": \"Dr. Paul De Lacost\", \"OpeningHours\": [ \"Mo 9.00 - 18.00\", \"Mo 9.00 - 18.00\", \"Mo 9.00 - 18.00\", \"Mo 9.00 - 18.00\", \"Mo 9.00 - 18.00\" ], \"Street\": \"Some street\", \"PostalCode\": 1204, \"City\": \"Berlin\", \"Longitude\": 13.3833, \"Latitude\": 52.5167\n}";
 	}
 }
 
--- AppointmentManager/Pages/DoctorDetailsPage/DoctorDetailsPage.xaml.cs
+++ AppointmentManager/Pages/DoctorDetailsPage/DoctorDetailsPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 
 namespace AppointmentManager
 {
@@ -11,6 +12,19 @@ namespace AppointmentManager
 		{
 			InitializeComponent ();
 			BindingContext = new DoctorDetailsPageViewModel (doctor);
+
+
+			// Xamarin maps don't support binding. We should add all element from here.
+			var position = new Position (doctor.Latitude, doctor.Longitude);
+			map.MoveToRegion(new MapSpan (position, 0.05, 0.05));
+
+			var pin = new Pin {
+				Type = PinType.Place,
+				Position = position,
+				Label = "Doctor",
+				Address = $"{doctor.Street} {doctor.PostalCode}"
+			};
+			map.Pins.Add(pin);
 		}
 	}
 }
