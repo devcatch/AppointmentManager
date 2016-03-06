cf270653d00dd307b4fe05925743ea48e5247db6
devcatch <bonde.art@gmail.com>
2016-03-06 10:02:18 +0100

    Added test for right init of view model

--- AppointmentManagerTest/ViewModels/AppointmentDetailsPageViewModelTest.cs
+++ AppointmentManagerTest/ViewModels/AppointmentDetailsPageViewModelTest.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using NUnit.Framework;
 using Moq;
+using AppointmentManager;
+using Xamarin.Forms;
 
 namespace AppointmentManagerTest
 {
 	[TestFixture]
 	public class AppointmentDetailsPageViewModelTest
-	{	
+	{
+		Mock<IDoctorService> _doctorInfoServiceMock;
+		Mock<INavigation> _navigationMock;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_doctorInfoServiceMock = new Mock<IDoctorService>();
+			_navigationMock = new Mock<INavigation>();
+		}
+
+		[Test]
+		public void ViewModelWasInitialisedCorrectly ()
+		{
+			// Arrange
+
+			// Act
+			var vm = new AppointmentDetailsPageViewModel (_doctorInfoServiceMock.Object, _navigationMock.Object, It.IsNotNull<Appointment>());
+
+			// Assert
+			Assert.IsTrue(vm.Time != null);
+			Assert.IsTrue(vm.Notes != null);
+		}
+
+
 	}
 }
 
