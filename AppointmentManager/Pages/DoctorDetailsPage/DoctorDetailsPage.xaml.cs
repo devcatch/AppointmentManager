7199fbbb5f086e707095a1251f20c20986a7d20b
devcatch <bonde.art@gmail.com>
2016-03-09 13:41:12 +0100

    Added map to android.

--- AppointmentManager/Pages/DoctorDetailsPage/DoctorDetailsPage.xaml.cs
+++ AppointmentManager/Pages/DoctorDetailsPage/DoctorDetailsPage.xaml.cs
@@ -8,21 +8,30 @@ namespace AppointmentManager
 {
 	public partial class DoctorDetailsPage : ContentPage
 	{
+		public Doctor Doctor {
+			get;
+			private set;
+		}
+
 		public DoctorDetailsPage (Doctor doctor)
 		{
 			InitializeComponent ();
+			Doctor = doctor;
 			BindingContext = new DoctorDetailsPageViewModel (doctor);
+		}
 
-
-			// Xamarin maps don't support binding. We should add all element from here.
-			var position = new Position (doctor.Latitude, doctor.Longitude);
+		protected override void OnAppearing ()
+		{
+			base.OnAppearing ();
+//			Xamarin maps don't support binding. We should add all element from here.
+			var position = new Position (Doctor.Latitude, Doctor.Longitude);
 			map.MoveToRegion(new MapSpan (position, 0.05, 0.05));
 
 			var pin = new Pin {
 				Type = PinType.Place,
 				Position = position,
 				Label = "Doctor",
-				Address = $"{doctor.Street} {doctor.PostalCode}"
+				Address = $"{Doctor.Street} {Doctor.PostalCode}"
 			};
 			map.Pins.Add(pin);
 		}
